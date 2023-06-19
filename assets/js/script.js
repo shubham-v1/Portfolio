d6a9d49016794e93ed3aa1071d8461596fc4aa10
Shubham Vairagade <shubhamvairagade91@gmail.com>
2023-06-20 01:06:10 +0530

    Main section is completed

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+// Stickey navbar
+const navbar = () =>{
+    let header = document.querySelector('.header');
+    window.addEventListener('scroll',()=>{
+        window.scrollY > 60 ?header.classList.add('headerColor'):header.classList.remove('headerColor');
+    })
+
+}
+
+// Dynamic Text code
+const dynamicTextFunction = () =>{
+    let dynamicElement = document.querySelector('.dynamic-text');
+    let cursor = document.querySelector('.cursor');
+    const textArray = ["web developer", "web designer"];
+    
+    const typingDelay = 200;
+    const erasingDelay = 100;
+    const newTextDelay = 2000;
+
+    let charIndex = 0;
+    let textArrayIndex = 0;
+
+    const type = () =>{
+        if(charIndex < textArray[textArrayIndex].length){
+            if(!cursor.classList.contains('typing')){
+                cursor.classList.add("typing");
+            }
+            dynamicElement.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type,typingDelay);
+        }
+        else{
+            cursor.classList.remove("typing");
+            setTimeout(erase,newTextDelay);
+        }
+    }
+    const erase = () =>{
+        if(charIndex > 0){
+            if(!cursor.classList.contains('typing')){
+                cursor.classList.add("typing");
+            }
+            dynamicElement.textContent = textArray[textArrayIndex].substring(0,charIndex - 1);
+            charIndex --;
+            setTimeout(erase, erasingDelay);
+        }
+        else{
+            cursor.classList.remove("typing");
+            textArrayIndex ++;
+            if(textArrayIndex >= textArray.length){
+                textArrayIndex = 0;
+            }
+            setTimeout(type,typingDelay + 1100);
+        }
+    }
+    if(textArray.length){
+        setTimeout(type,newTextDelay+250);
+    }
+}
+
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    dynamicTextFunction();
+    navbar();
+})
